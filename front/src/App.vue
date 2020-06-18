80dba3479cd8661779eff4661681bb4eb40a9a3c
gaberani <khs0783@naver.com>
2020-06-18 19:02:21 +0900

    css design

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,32 +1,46 @@
 <template>
- <div id="app">
+ <div id="app" class="container">
     <div id="nav">
-      <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-        <router-link to="/" class="mr-3">MoVueHome</router-link>
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <router-link to="/" class="logo">MoVueHome</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="movie-search">
+              영화검색 : <input type="text" v-model="movietitle">
+              <button @click="send" @keyup.enter="send">검색</button>
+              <br>
+              <br>
+              <ul v-for="movie in movielist" :key="movie.id" :movie="movie">
+                <li>
+                  {{movie.title}}
+                </li>
+                <div class="img-box" @click="detailmovie(movie)">
+                  <img :src="PosterUrl(movie)">
+                </div>
+              </ul>
+          </div>
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-              <router-link to="/articles">게시글 목록</router-link>
+              <router-link to="/articles">글 목록</router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/movies/SearchList">Search</router-link>
+              <router-link to="/movies/SearchList">장르 검색</router-link>
             </li>
-            <li v-if="!islogin" class="nav-item mx-2">
+            <li v-if="!islogin" class="nav-item ">
               <router-link to="/accounts/login">로그인</router-link>
             </li>
-            <li v-if="!islogin" class="nav-item mx-2">
+            <li v-if="!islogin" class="nav-item ">
               <router-link to="/accounts/signup">회원가입</router-link>
             </li>
-            <li v-if="islogin" class="nav-item mx-2">
-              <router-link to="/articles/create">게시글 쓰기</router-link>
+            <li v-if="islogin" class="nav-item ">
+              <router-link to="/articles/create">글 쓰기</router-link>
             </li>
-            <li v-if="islogin" class="nav-item mx-2">
-              <router-link to="/movies/MovieRecommend">사용자용 추천</router-link>
+            <li v-if="islogin" class="nav-item ">
+              <router-link to="/movies/MovieRecommend">영화 추천</router-link>
             </li>
-            <li v-if="islogin" class="nav-item mx-2">
+            <li v-if="islogin" class="nav-item ">
               <router-link to="/accounts/logout" @click.native="logout">로그아웃</router-link>
             </li>
           </ul>
@@ -49,9 +63,29 @@ export default{
   data(){
     return{
       islogin:false,
+      movies:null,
+      movietitle:null,
+      movielist:[],
+      movieData:{}
     }
   },
   methods:{
+    send(){
+      axios.get(`${SERVER_URL}/movies/search/${this.movietitle}/`)
+        .then((res)=>{
+          console.log(res.data)
+          if (res.data['message']){
+            alert(`Message:${res.data['message']}`)
+          }
+          else{
+            this.movielist=res.data
+          }
+        })
+        .catch(err=>console.log(err))
+    },
+    PosterUrl(one){
+      return "https://image.tmdb.org/t/p/w300/" + one.poster_path
+    },
     setCookie(token){
       this.$cookies.set('auth-token',token)
       this.islogin=true
@@ -94,6 +128,12 @@ export default{
 </script>
 
 <style>
+    body {
+        background-color:#000;
+    }
+    a:hover {
+        text-decoration: none;
+    }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -103,19 +143,51 @@ export default{
 }
 
 #nav {
-  padding: 0px;
+  position:fixed; left:0; top:0;
+  width:100%;
+  padding: 0;
+  z-index:1
+}
+.navbar {
+  background-color:#000;
+}
+.logo {
+  position:absolute; top:22px; left:25px;
+    font-size:26px; color:#fff
+}
+.movie-search {
+  text-align:center;
+    margin: 0 auto;
+}
+.movie-search input{
+ background-color:transparent;
+  border:0;
+  border-bottom:2px solid rgb(144,144,144);
+  padding:10px 5px;
+    width:500px;
+
+}
+.navbar-nav {
+    position:absolute;
+    top:27px; right:0;
 }
 
-#nav a {
+.nav-item a {
+    color:rgb(144,144,144);
+    font-size:19px;
+    padding:20px 10px;
+}
+#nav a:hover {
   font-weight: bold;
-  color: #2c3e50;
+  color:#fff;
 }
-
 #nav a.router-link-exact-active {
-  color: #2ee492;
+  color: #fff;
 }
 
 .container {
   margin: 0
 }
+
+
 </style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,69 +1,28 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        TITLE : <input type="text" v-model="movietitle">
-        <button @click="send" @keyup.enter="send">검색</button>
-        <br>
-        <br>
-        <ul v-for="movie in movielist" :key="movie.id" :movie="movie">
-          <li>
-            {{movie.title}}
-          </li>
-          <div class="img-box" @click="detailmovie(movie)">
-            <img :src="PosterUrl(movie)">
-          </div>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <h1>오늘의 추천</h1>
-        <MainView/>
-        <br>
-    </div>
+  <div class="main-box">
+    <h1 class="tit-main">오늘의 추천</h1>
+      <MainView/>
+      <br>
   </div>
 </template>
 
 <script>
 import MainView from '@/views/movies/MainView.vue'
 
-
-import axios from 'axios'
-const SERVER_URL = "http://localhost:8000"
-
 export default {
   name: 'Home',
   components: {
     MainView,
   },
-  data(){
-    return{
-      movies:null,
-      movietitle:null,
-      movielist:[],
-      movieData:{}
-    }
-  },
-  methods:{
-    send(){
-      axios.get(`${SERVER_URL}/movies/search/${this.movietitle}/`)
-      .then((res)=>{
-        console.log(res.data)
-        if (res.data['message']){
-          alert(`Message:${res.data['message']}`)
-        }
-        else{
-          this.movielist=res.data
-        }
-        
-      })
-      
-      .catch(err=>console.log(err))
-      
-    },
-    PosterUrl(one){
-      return "https://image.tmdb.org/t/p/w300/" + one.poster_path
-    },
-  }
 }
 </script>
+
+<style>
+  .main-box {
+    padding-top:84px;
+  }
+  .tit-main {
+    margin:60px 0 40px;
+    font-weight:bold; color:#fff
+  }
+</style>
